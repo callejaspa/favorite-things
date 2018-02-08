4db20d013b61307d5c0b37fc1c25ba20db7709ca
Pauline Callejas <callejaspa@gmail.com>
2018-02-08 13:29:00 -0500

    Local only favorite color

--- FavoriteThings/src/app/app.component.ts
+++ FavoriteThings/src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title: string = 'Favorite Things';
+  favoriteColor: string = 'pink';
+
+  setColor(selectedColor: string): void{
+    console.log("You selected the color " + selectedColor);
+    this.favoriteColor = selectedColor;
+  }
+
+  updateColor(): void {
+    console.log("TODO:Update the color");
+  }
 }
